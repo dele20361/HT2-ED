61ee77585cc17bef557ed76a66be86f40823b805
dele20361 <64100118+dele20361@users.noreply.github.com>
2021-02-06 22:18:50 -0600

    Javadoc

---
+++ Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calcu"},{"p":"<Unnamed>","l":"calculadora"},{"p":"<Unnamed>","l":"IStack"},{"p":"<Unnamed>","l":"Operaciones"},{"p":"<Unnamed>","l":"stack"}]
